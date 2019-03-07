342d0eb7944c1428ea905ecd8b35a709b57eb522
GQSM <ms314006@hotmail.com>
2019-03-08 00:45:02 +0800

    victory

--- src/index.js
+++ src/index.js
@@ -6,15 +6,12 @@ const Main = () => {
     const [ws, setWs] = useState(null)
 
     const connectWebSocket = () => {
-        //開啟
         setWs(webSocket('http://localhost:3000'))
     }
 
     useEffect(() => {
         if (ws) {
-            //連線成功在 console 中打印訊息
             console.log('success connect!')
-            //設定監聽
             initWebSocket()
         }
     }, [ws])
@@ -30,14 +27,30 @@ const Main = () => {
         ws.on('getMessageLess', message => {
             console.log(message)
         })
+        ws.on('addRoom', message => {
+            console.log(message)
+        })
     }
 
     const sendMessage = (name) => {
         ws.emit(name, '收到訊息囉！')
     }
 
+    const changeRoom = (event) => {
+        let room = event.target.value
+        if(room !== ''){
+            ws.emit('addRoom', room)
+        }
+    }
+
     return (
         <div>
+            <select onChange={changeRoom}>
+                <option value=''>請選擇房間</option>
+                <option value='room1'>房間一</option>
+                <option value='room2'>房間二</option>
+            </select>
+            {/*其餘省略*/}
             <input type='button' value='連線' onClick={connectWebSocket} />
             <input type='button' value='送出訊息，只有自己收到回傳' onClick={() => { sendMessage('getMessage') }} />
             <input type='button' value='送出訊息，讓所有人收到回傳' onClick={() => { sendMessage('getMessageAll') }} />
